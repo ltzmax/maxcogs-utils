e428117125fb580e847ad3feb77ee8a40a55b5f7
MAX <63972751+ltzmax@users.noreply.github.com>
2026-04-24 11:56:36 +0200

    yes

--- dcv2nav/__init__.py
+++ dcv2nav/__init__.py
@@ -4,9 +4,9 @@ dcv2nav - Discord Components v2 navigation views for Red-DiscordBot cogs.
 Provides drop-in paginator views compatible with discord.py 2.x LayoutView.
 """
 
-from .paginator import LayoutViewPaginator
-from .select_paginator import SelectPaginator
+from .paginator import LayoutViewPaginator, _NavBtn
+from .select_paginator import SelectPaginator, _SelectPaginatorSelect
 
-__all__ = ["LayoutViewPaginator", "SelectPaginator"]
-__version__ = "1.0.0"
+__all__ = ["LayoutViewPaginator", "SelectPaginator", "_NavBtn", "_SelectPaginatorSelect"]
+__version__ = "1.2.0"
 __author__ = "ltzmax"
